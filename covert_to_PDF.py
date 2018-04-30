3a1a538fcf2c11ddf95484093c03301e8a117538
Karan Sawhney <karsaw@users.noreply.github.com>
2018-04-30 19:43:19 +0530

    Add files via upload

---
+++ covert_to_PDF.py
@@ -0,0 +1,12 @@
+import pdfkit
+import sys
+options = {
+    'page-size': 'A4',
+    'margin-top': '0.75in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '0.75in',
+}
+
+
+pdfkit.from_file('test.html', 'test.pdf')
